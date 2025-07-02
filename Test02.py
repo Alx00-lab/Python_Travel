384e75569466517a8ee4b9b98eb5daf78830cf27
Alx00 <almontealexander345@gmail.com>
2025-07-02 16:00:06 -0400

    First commit

---
+++ Test02.py
@@ -0,0 +1,9 @@
+#trying some code...
+
+correos = ['ejemplo1@gmail.com', 
+           'ejemplo2@dominio.com', 
+           'ejemplo3@hotmail.com', 
+           'ejemplo4@dominio.com']
+
+# Imprimir la lista de correos donde sean solo correos de dominio.com
+print([correo for correo in correos if '@dominio.com' in correo])
